f6f0c0d5c5652c84d00ecd2dcb58c53381fefd7a
Przemyslaw <przemyslaw.szubartowski@o2.pl>
2023-06-17 22:24:54 +0200

    Created three addEventListener, added new function: handlePres and sybmitPress.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-console.log(import.meta.env.VITE_ACCESS_KEY)
-
 const API_URL = import.meta.env.VITE_API_URL
 const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY
 
-;(() => {
-	fetch(`${API_URL}/search/photos/?client_id=${ACCESS_KEY}&query=dog`)
+const inputSearch = document.querySelector('.hero_centered-form-input');
+const selectElement = document.getElementById('hero_select');
+const selectedValue = selectElement.value;
+const submitBtn = document.querySelector('.hero_centered-form-btn')
+
+
+
+
+const searchPhoto = ()  => {
+	fetch(`${API_URL}/search/photos/?client_id=${ACCESS_KEY}&query=${inputSearch.value}`)
 		.then(res => res.json())
 		.then(data => console.log(data))
-})()
+
+		
+}
+
+const handleSubmit = (e) => {
+	e.preventDefault();
+	searchPhoto();
+}
+
+const handleKeyPress = event => {
+	if (event.key === 'Enter') {
+	  event.preventDefault();
+	  searchPhoto();
+	}
+  };
+
+
+
+inputSearch.addEventListener('change', searchPhoto)
+inputSearch.addEventListener('keydown', handleKeyPress)
+submitBtn.addEventListener('click', handleSubmit)
