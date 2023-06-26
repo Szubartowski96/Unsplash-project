beef22a7ef39c3f2bc7ed4b91d22c627f6c17408
Przemyslaw <przemyslaw.szubartowski@o2.pl>
2023-06-26 22:48:44 +0200

    created linkElement and imgElement

--- js/main.js
+++ js/main.js
@@ -5,14 +5,29 @@ const inputSearch = document.querySelector('.hero_centered-form-input');
 const selectElement = document.getElementById('hero_select');
 const selectedValue = selectElement.value;
 const submitBtn = document.querySelector('.hero_centered-form-btn')
+const photosContainer = document.querySelector('.photos')
 
 
 
 
 const searchPhoto = ()  => {
+	photosContainer.innerHTML='';
 	fetch(`${API_URL}/search/photos/?client_id=${ACCESS_KEY}&query=${inputSearch.value}`)
 		.then(res => res.json())
-		.then(data => console.log(data))
+		.then(data => {
+
+			data.results.forEach(photo => {
+
+				const linkElement = document.createElement('a');
+				linkElement.href = photo.links.html;
+				const imgElement = document.createElement('img');
+				imgElement.src = photo.urls.regular;
+				imgElement.alt = photo.alt_description;
+
+				linkElement.appendChild(imgElement);
+				photosContainer.appendChild(linkElement);
+			});
+		})
 
 		
 }
@@ -29,6 +44,7 @@ const handleKeyPress = event => {
 	}
   };
 
+ 
 
 
 inputSearch.addEventListener('change', searchPhoto)
