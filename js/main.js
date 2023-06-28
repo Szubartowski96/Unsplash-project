f6cd6116da155e1eb6b49c0a2c5b528eb46e0254
Przemyslaw <przemyslaw.szubartowski@o2.pl>
2023-06-28 23:45:34 +0200

    created new select with count of photos, created new function in JS

--- js/main.js
+++ js/main.js
@@ -3,34 +3,50 @@ const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY
 
 const inputSearch = document.querySelector('.hero_centered-form-input');
 const selectElement = document.getElementById('hero_select');
-const selectedValue = selectElement.value;
 const submitBtn = document.querySelector('.hero_centered-form-btn')
 const photosContainer = document.querySelector('.photos')
+const countPage = document.querySelector('.hero_count-of-page')
 
+let perPage = 10;
+
+
+const createElement = (data) => {
+	data.results.forEach(photo => {
+
+		const linkElement = document.createElement('a');
+		linkElement.href = photo.links.html;
+		linkElement.target = '_blank';
+		linkElement.addEventListener('click', (e) => {
+  		e.preventDefault();
+		});
+		const imgElement = document.createElement('img');
+		imgElement.src = photo.urls.regular;
+		imgElement.alt = photo.alt_description;
+		linkElement.appendChild(imgElement);
+		photosContainer.appendChild(linkElement);
+		
+	});
+}
 
 
 
 const searchPhoto = ()  => {
 	photosContainer.innerHTML='';
-	fetch(`${API_URL}/search/photos/?client_id=${ACCESS_KEY}&query=${inputSearch.value}`)
+	const count = parseInt(selectElement.value);
+  perPage = count;
+	fetch(`${API_URL}/search/photos/?client_id=${ACCESS_KEY}&query=${inputSearch.value}&per_page=${count}`)
 		.then(res => res.json())
 		.then(data => {
 
-			data.results.forEach(photo => {
 
-				const linkElement = document.createElement('a');
-				linkElement.href = photo.links.html;
-				const imgElement = document.createElement('img');
-				imgElement.src = photo.urls.regular;
-				imgElement.alt = photo.alt_description;
-
-				linkElement.appendChild(imgElement);
-				photosContainer.appendChild(linkElement);
-			});
+			createElement(data);
+			
 		})
 
 		
 }
+
+
 
 const handleSubmit = (e) => {
 	e.preventDefault();
@@ -50,3 +66,4 @@ const handleKeyPress = event => {
 inputSearch.addEventListener('change', searchPhoto)
 inputSearch.addEventListener('keydown', handleKeyPress)
 submitBtn.addEventListener('click', handleSubmit)
+selectElement.addEventListener('change', searchPhoto);
