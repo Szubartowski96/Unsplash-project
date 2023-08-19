047a548fcda82ff0c8ca15728a48df7fcd5efc1a
Przemyslaw <przemyslaw.szubartowski@o2.pl>
2023-08-19 23:15:00 +0200

    added new functionalities

--- js/main.js
+++ js/main.js
@@ -5,14 +5,16 @@ const inputSearch = document.querySelector('.hero_centered-form-input')
 const selectElement = document.getElementById('hero_select')
 const submitBtn = document.querySelector('.hero_centered-form-btn')
 const photosContainer = document.querySelector('.photos')
-const countPage = document.querySelector('.hero_count-of-page')
 const warning = document.querySelector('.hero_centered--warning')
 const closeModal = document.querySelector('.modal_content-close')
 const modal = document.querySelector('.modal')
 const modalImage = document.querySelector('.modal_img')
 const selectSize = document.getElementById('select_choose-size')
+const dowloadBtn = document.querySelector('.modal_download-button')
 
 let perPage = 0
+let photosResults
+let isImageLoaded = false
 
 const createElement = data => {
 	data.results.forEach((photo, index) => {
@@ -31,7 +33,6 @@ const createElement = data => {
 		photosContainer.appendChild(linkElement)
 	})
 }
-let photosResults
 
 const searchPhoto = () => {
 	if (selectElement.value === '0') {
@@ -53,7 +54,8 @@ const searchPhoto = () => {
 			photosResults = data.results
 			createElement(data)
 			inputSearch.value = ''
-			
+			photosResults.value = ''
+			selectElement.value = '0'
 		})
 }
 
@@ -81,28 +83,85 @@ const handleKeyPress = e => {
 
 const closeModalBtn = () => {
 	modal.style.display = 'none'
+	modalImage.src = ''
+	modalImage.id = ''
+	isImageLoaded = false
 }
 const openModalBtn = e => {
 	const { id: imgId } = e.target
 	modal.style.display = 'flex'
-	const imageUrl = photosResults[imgId].urls.small
-	modalImage.src = imageUrl
 	modalImage.id = imgId
+	selectSize.value = '0'
 
-	
+	const clickedImg = document.getElementById(imgId)
+	const altDescription = clickedImg.alt
+
+	modalImage.alt = altDescription
 }
 
 const showPhotoInModal = () => {
 	const { value: selectedSize } = selectSize
-	const imageUrl = photosResults[modalImage.id].urls[selectedSize]
+	const imgId = modalImage.id
+
+	if (
+		!photosResults ||
+		imgId < 0 ||
+		imgId >= photosResults.length ||
+		!photosResults[imgId].urls ||
+		!photosResults[imgId].urls[selectedSize]
+	) {
+		return
+	}
+
+	const imageUrl = photosResults[imgId].urls[selectedSize]
 	modalImage.src = imageUrl
-	
+	isImageLoaded = true
+}
+const showButtonOnPhoto = () => {
+	dowloadBtn.style.display = 'flex'
+	modalImage.style.opacity = '0.6'
+}
+
+const hideButtonOnPhoto = () => {
+	dowloadBtn.style.display = 'none'
+	modalImage.style.opacity = '1'
+}
+
+const downloadImage = imageUrl => {
+	const link = document.createElement('a')
+	link.href = imageUrl
+	link.target = '_blank'
+	link.download = 'zdjecie.jpg'
+
+	link.click()
 }
 
 inputSearch.addEventListener('change', searchPhoto)
 inputSearch.addEventListener('keydown', handleKeyPress)
 submitBtn.addEventListener('click', handleSubmit)
-selectElement.addEventListener('change', searchPhoto)
 closeModal.addEventListener('click', closeModalBtn)
 photosContainer.addEventListener('click', openModalBtn)
 selectSize.addEventListener('change', showPhotoInModal)
+
+modalImage.addEventListener('mousemove', function (event) {
+	const modalImageRect = modalImage.getBoundingClientRect()
+	const positionX = event.clientX - modalImageRect.left
+	const positionY = event.clientY - modalImageRect.top
+	const { value: selectedSize } = selectSize
+
+	let conditionMet = false
+
+	if (selectedSize === 'small') {
+		conditionMet = positionX > 100 && positionX < 330 && positionY > 80 && positionY < 200
+	} else if (selectedSize === 'regular') {
+		conditionMet = positionX > 100 && positionX < 600 && positionY > 80 && positionY < 300
+	}
+
+	if (conditionMet) {
+		showButtonOnPhoto()
+	} else {
+		hideButtonOnPhoto()
+	}
+})
+
+dowloadBtn.addEventListener('click', downloadImage)
